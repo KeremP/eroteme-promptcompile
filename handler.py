b590d3c1f503fe2488e76d5b8cdd751028e1ccbe
KeremP <kerpro26@gmail.com>
2023-02-03 14:59:27 -0500

    return sources in response

--- handler.py
+++ handler.py
@@ -33,7 +33,7 @@ def lambda_handler(event, context):
 
     prompt = build_prompt(results, query)
 
-    resp = {'prompt':prompt}
+    resp = {'prompt':prompt, 'sources':sources}
     return resp
     
     
